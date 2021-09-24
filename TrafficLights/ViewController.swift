facba3dd190b3e204bb5f53978e7fc3f9b0881fe
iForneu <ikorotaevkirill@gmail.com>
2021-09-24 23:52:09 +0300

    Navigation on ViewController

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 
+//MARK: - Enumeration lights
 enum CurrentLight {
     case red, yellow, green
 }
 
+//MARK: - Outlets
 class ViewController: UIViewController {
 
     @IBOutlet weak var redLight: UIView!
@@ -18,11 +20,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenLight: UIView!
     
     @IBOutlet weak var startButton: UIButton!
-    
+
+//MARK: - Private functions
     private var currentLight = CurrentLight.red
     private let lightIsOn: CGFloat = 1.0
     private let lightIsOff: CGFloat = 0.3
     
+//MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,13 +36,15 @@ class ViewController: UIViewController {
         yellowLight.alpha = lightIsOff
         greenLight.alpha = lightIsOff
     }
-    
+
+//MARK: - viewDidLayoutSubviews
     override func viewDidLayoutSubviews() {
         redLight.layer.cornerRadius = redLight.frame.width / 2
         yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
         greenLight.layer.cornerRadius = greenLight.frame.width / 2
     }
     
+//MARK: - IBAction
     @IBAction func startButtonPressed() {
         if startButton.currentTitle == "START" {
             startButton.setTitle("NEXT", for: .normal)
